c49edb046a80d37e2e671c082e556a3497316959
Christelle Regazzoni <cristel.regaz@gmail.com>
2023-05-03 08:52:17 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -28,13 +28,62 @@ const oppoStatus = [
 
 const FormComponent = class {
   constructor() {
+    /*access select element */
+    this.select = document.querySelector("select[name='status']");
+    /*listen to change on select element */
+    this.select.addEventListener("change", (e) =>
+      this.onChange(e)
+    );
 
+    /*access form */
+    const form = document.forms[0];
+    /*listen to form submit */
+    form.addEventListener("submit", (e) => this.onSubmit(e));
   }
+
+  onChange(e) {
+    /* find the right oppo */
+    const opportunity = this.find(e.target.value);
+    /* get the input element */  
+    const success = document.querySelector("input[name='success']");
+    /* add the value of success to it */
+    success.value = opportunity.SUCCESS;
+  };
+
   start() {
-    // Start modifying the form elements here!
-    // You are allowed to add extra methods, properties or change the constructor of this class
+    const select = document.getElementsByName('status');
+    /* loop through all oppo status from array */
+    oppoStatus.forEach((e) => {
+      /* create list of options */
+      const option = new Option(e.STATUS, e.STATUS)
+      /* append list to select element */
+      select[0].appendChild(option);
+    });
+  };
+
+  onSubmit(e) {
+    /* block default click handling */
+    e.preventDefault();
+    /* find right oppo status */
+    const opportunity = this.find(this.select.value);
+    /* get output element */
+    const outputElement = document.querySelector(".output");
+    /* get matching status and success */
+    const output = {
+      status: opportunity.K_OPPO_STATUS,
+      success: opportunity.SUCCESS,
+    };
+    /* output the form element values as JSON string */
+    outputElement.innerHTML = JSON.stringify(output);
   }
-}
+
+  find(status) {
+    return oppoStatus.find(
+      (option) => option.K_OPPO_STATUS === parseInt(status)
+    );
+  }
+
+};
 
 const fc = new FormComponent();
 fc.start();
